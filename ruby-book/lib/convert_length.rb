1fd7dcf977170b7240451485ba43bfab3b9cc051
nekomaho <nekosatoru@gmail.com>
2018-02-25 21:52:09 +0900

    5.1.2 my implementation

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,2 +1,10 @@
-def convert_length(number, convert_unit_from, convert_unit_to)
+CONVERT_UNIT = { 'm>in' => '*39.37',
+                 'm>ft' => '*3.28',
+                 'in>m' => '/39.37',
+                 'ft>m' => '/3.28',
+                 'ft>in' => '*12.00',
+                 'in>ft' => '/12.00'}.freeze
+def convert_length(number, from, to)
+  calc = CONVERT_UNIT["#{from}>#{to}"]
+  eval("#{number}#{calc}").round(2)
 end
